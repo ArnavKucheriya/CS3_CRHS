db80867aeeb32e2cc9c5819399040f9312ab2eae
ArnavKucheriya <ArnavKucheriya@outlook.com>
2023-04-28 08:53:47 -0500

    Adding Percolation Lab
    Adding Percolation Lab from Princeton's Algorithm Course of Coursera.

---
+++ Percolation/src/Demo.java
@@ -0,0 +1,25 @@
+/**
+ * @(#)Demo.java
+ *
+ *
+ * @author 
+ * @version 1.00 2016/12/18
+ */
+
+public class Demo {
+        
+    /**
+     * Creates a new instance of <code>Demo</code>.
+     */
+    public Demo() {
+    	
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hello "+ args[0] );
+    }
+}
